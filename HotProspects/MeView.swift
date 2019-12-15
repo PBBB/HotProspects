d7bbbc82aa28cad5b0a17349c8ad89e4e994fcf9
Issac Penn <pbbsignup@outlook.com>
2019-12-15 12:17:31 +0800

    Day 82 done

---
+++ HotProspects/MeView.swift
@@ -0,0 +1,21 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Issac Penn on 2019/12/15.
+//  Copyright © 2019 Issac Penn. All rights reserved.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("ME")
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
